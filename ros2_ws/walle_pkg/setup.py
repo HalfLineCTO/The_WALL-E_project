821ceb63443083e479df7391765630c33982d00d
Juan Esteban <je.cardona@uniandes.edu.co>
2023-04-19 12:01:55 -0500

    git push -m

--- ros2_ws/walle_pkg/setup.py
+++ ros2_ws/walle_pkg/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'Walle_pkg'
+package_name = 'walle_pkg'
 
 setup(
     name=package_name,
@@ -20,9 +20,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'turtle_bot_teleop = Walle_pkg.turtle_bot_teleop:main'
-            'connectionSerial = Walle_pkg.connection_serial:main'
-            'turtle_bot_interface = Walle_pkg.walle_interface:main'
+            'turtle_bot_teleop = walle_pkg.turtle_bot_teleop:main'
+            'connectionSerial = walle_pkg.connection_serial:main'
+            'turtle_bot_interface = walle_pkg.walle_interface:main'
             
         ],
     },
